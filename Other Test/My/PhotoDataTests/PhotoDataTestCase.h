409c850496f4dd591949e81df35c2f6249c8d1ba
YSC <yuanshichong@wowsai.com>
2015-08-28 18:03:55 +0800

    Testing///

---
+++ Other Test/My/PhotoDataTests/PhotoDataTestCase.h
@@ -0,0 +1,20 @@
+//
+//  PhotoDataTestCase.h
+//  PhotoData
+//
+//  Created by YSC on 15/8/28.
+//  Copyright (c) 2015年 objc.io. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
+@interface PhotoDataTestCase :XCTestCase
+
+- (void)setUp; NS_REQUIRES_SUPER;
+
+- (void)tearDown; NS_REQUIRES_SUPER;
+
+
+
+
+@end
---
+++ Other Test/My/PhotoDataTests/PhotoDataTestCase.m
@@ -0,0 +1,13 @@
+//
+//  PhotoDataTestCase.m
+//  PhotoData
+//
+//  Created by YSC on 15/8/28.
+//  Copyright (c) 2015年 objc.io. All rights reserved.
+//
+
+#import "PhotoDataTestCase.h"
+
+@implementation PhotoDataTestCase
+
+@end
